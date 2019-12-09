2c9db0a3f72b9baad5a9e70a707fcda51d9390b9
Kim Sanghyeok <skyee23@gmail.com>
2019-12-09 21:20:43 +0900

    서버 URL 수정

--- plugins/socket.js
+++ plugins/socket.js
@@ -2,6 +2,6 @@ import Vue from 'vue';
 import Stomp from 'vue-stomp';
 
 let api_endpoint = 'http://ec2-18-223-151-196.us-east-2.compute.amazonaws.com:8080';
-let local_endpoint = 'http://localhost:8080';
+let local_endpoint = 'http://actdev.i-on.net:18080';
 
 Vue.use(Stomp, `${local_endpoint}/ws`);
